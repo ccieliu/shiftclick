c962dc049b86ebd453b13bc2993671687c035733
Bruce Liu -X (yuxuliu - DLSP at Cisco) <yuxuliu@cisco.com>
2019-06-12 18:29:26 +0800

    Fix the token function issue

--- notice.py
+++ notice.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 from concurrent.futures import ThreadPoolExecutor
 from email.mime.text import MIMEText
-from teamBotToken import teamBotToken
+from teamBotToken import BotToken
 executor = ThreadPoolExecutor(2)
 
 
@@ -13,10 +13,9 @@ executor = ThreadPoolExecutor(2)
 # <@personEmail:yuxuliu@cisco.com>
 # <@all>
 
-
 class alerter(object):
     def __init__(self):
-        self.accessToken = teamBotToken.token
+        self.accessToken = BotToken().token
         self.botId = "Y2lzY29zcGFyazovL3VzL0FQUExJQ0FUSU9OL2Q3NzQ5MmY1LTFlNGYtNGJhZC04OTAzLTI0MDVlNGFkNmFkYQ"
         self.FTDroomid = "Y2lzY29zcGFyazovL3VzL1JPT00vMDZiMDU1ODAtOGI0My0xMWU5LWFiYmUtOTcyMDY5ZDAzNDY0"
         self.ASAroomid = "Y2lzY29zcGFyazovL3VzL1JPT00vZDA4YTYzNzAtOGNmNC0xMWU5LWIyY2EtODdjODEwZjA3MTU2"
